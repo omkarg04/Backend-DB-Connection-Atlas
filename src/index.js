6c785849545a42e2e9687ae12d1747ea1dd5d470
omkarg04 <og628851@gmail.com>
2026-01-23 12:20:54 +0530

    completed utils for async handling and API errors.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,14 @@ dotenv.config({
 });
 
 connectDB();
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at  port : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Error connecting to the database", error);
+})
 
 
 
