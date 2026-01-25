db3631b5aa2d362bb4a6d8ff56ab31af13f0f4db
omkarg04 <og628851@gmail.com>
2026-01-25 18:42:35 +0530

    add user and  video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true,
     },
-    fullname :{
+    fullName :{
         type :String,
         required: true,
         trim: true,
@@ -48,6 +48,44 @@ const userSchema = new Schema({
 
     }
 },{timestamps: true});
+
+
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password") ) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next() 
+    
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign()
+    {
+        _id = this._id,
+        email = this.email,
+        username = this.username,
+        fullName = this.fullName
+    } 
+    process.env.ACCESS_TOKEN_SECRET ,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+}
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign()
+    {
+        _id = this._id
+        
+    } 
+    process.env.REFRESH_TOKEN_SECRET ,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+}
+
 
 export const User = mongoose.model("User",userSchema);
 
