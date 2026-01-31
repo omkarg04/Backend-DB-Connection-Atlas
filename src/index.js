f44104d95b1baa8fa1c75fb6130b3fc017deb0b9
omkarg04 <og628851@gmail.com>
2026-01-31 23:40:05 +0530

    started learning controller guide

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 // require('dotenv').config({path: './.env'});
-import dotenv from "dotenv";    
+import dotenv from "dotenv";
 
 
 
 
-import mongoose from "mongoose";    
-import { DB_NAME } from "./constants.js";  
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
 
 dotenv.config({
-    path: './.env' 
+    path: './.env'
 });
 
-connectDB();
-.then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at  port : ${process.env.PORT}`)
     })
 })
-.catch((error)=>{
-    console.log("Error connecting to the database", error);
-})
+    .catch((error) => {
+        console.log("Error connecting to the database", error);
+    })
 
-
+  
 
 
 
